eb9a6b8a3c78b3ed1038601fa1c66cab0ae8939c
domecraft <christopherdurr1@gmail.com>
2013-11-07 09:16:06 -0800

    made changes to fix several errors
    made c a string so it could concatenate with the print statement. also imported the math function.

--- Mathematics/Geometry/Pythagorean Solver.py
+++ Mathematics/Geometry/Pythagorean Solver.py
@@ -1,15 +1,17 @@
+import math
 print"This program will solve the pythagorean theorem for you"
 unit=raw_input('Enter the unit you will be using')
 a=raw_input('Enter the length of side a')
+a=float(a)
 if a<0:
   print"A cannot be less than zero"
-else
-a=float(a)
-b=raw_input('Enter the length of side b')
+else:
+    b=raw_input('Enter the length of side b')
+    b=float(b)
 if b<0:
   print"B cannot be less than zero"
-else
-b=float(b)
-c2=(a**2)+(b**2)
-c=math.sqrt(c2)
-print "The length of side C is: "+ c + " " + unit + "."
+else:
+    c2=(a**2)+(b**2)
+    c=math.sqrt(c2)
+    c=str(c)
+    print "The length of side C is: "+ c + " " + unit + "."
