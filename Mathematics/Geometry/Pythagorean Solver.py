51908a6c078a02b135d47ba70afbddfeb708aaf3
domecraft <christopherdurr1@gmail.com>
2013-11-07 12:50:00 -0800

    Got rid of variable C2
    Variable c2 was deleted and I just replaced it with doing the entire calculation on one line.

--- Mathematics/Geometry/Pythagorean Solver.py
+++ Mathematics/Geometry/Pythagorean Solver.py
@@ -1,16 +1,15 @@
 import math
 print"This program will solve the pythagorean theorem for you"
-unit=raw_input('Enter the unit you will be using')
-a=float(raw_input('Enter the length of side a'))
+unit=raw_input('Enter the unit you will be using: ')
+a=float(raw_input('Enter the length of side a: '))
 if a<0:
   print"A cannot be less than zero"
 else:
-    b=float(raw_input('Enter the length of side b'))
+    b=float(raw_input('Enter the length of side b: '))
     if b<0:
         print"B cannot be less than zero"
     else:
-        c2=(a**2)+(b**2)
-        c=math.sqrt(c2)
+        c=math.sqrt((a**2)+(b**2))
         c=str(c)
         print "The length of side C is: "+ c + " " + unit + "."
     
