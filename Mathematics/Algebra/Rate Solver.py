7f7165ebaf97069c7fc3ecc1769edb6b509735e6
Christopher Durr <christopherdurr1@gmail.com>
2013-11-27 21:09:50 -0800

    Improved formatting of + signs
    + signs were too cramped so I added spaces in between them

--- Mathematics/Algebra/Rate Solver.py
+++ Mathematics/Algebra/Rate Solver.py
@@ -3,4 +3,4 @@ unit=raw_input("Enter the units of measurement: ")
 distance=float(raw_input("Enter the total distance: " ))
 time=float(raw_input("Enter the total time in hours: "))
 rate=str(distance/time)
-print "The rate is " + rate + " "+unit+" an hour"
+print "The rate is " + rate + " " + unit + " an hour"
