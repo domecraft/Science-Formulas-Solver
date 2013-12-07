107022d515b45b9b85ba38c9c28e9f386f7e658f
Christopher Durr <christopherdurr1@gmail.com>
2013-12-07 15:06:13 -0800

    Create Power Solver.rb

---
+++ Mathematics/Algebra/Power Solver.rb
@@ -0,0 +1,13 @@
+#This program will solve for the powers and loops through the range that the user enters.
+
+range = []
+puts "Enter the minimum value of the loop"
+min = gets.chomp
+min = min.to_i
+puts "Enter the maximum value of the loop"
+max = gets.chomp
+max = max.to_i
+puts "Enter the power you want to apply to each number"
+power = gets.chomp
+power = power.to_i
+(min..max).each{|x| print "#{x}:", x**power, " "}
